c77b2c83987d131debf6599028e463c6ade89f61
yarenty <yarenty@gmail.com>
2017-10-05 16:18:36 +0100

    XGB fix

--- src/main/scala/com/yarenty/h2o/ModelPrepariaiton.scala
+++ src/main/scala/com/yarenty/h2o/ModelPrepariaiton.scala
@@ -2,11 +2,13 @@ package com.yarenty.h2o
 
 import java.net.URI
 
+import hex.Model.Parameters.CategoricalEncodingScheme
+import hex.ScoreKeeper.StoppingMetric
 import hex.deeplearning.{DeepLearning, DeepLearningModel}
 import hex.deeplearning.DeepLearningModel.DeepLearningParameters
 import hex.tree.xgboost.{XGBoost, XGBoostModel}
 import hex.tree.xgboost.XGBoostModel.XGBoostParameters
-import water.fvec.H2OFrame
+import water.fvec.{H2OFrame, Vec}
 
 /**
   * Created by yarenty on 20/06/17.
@@ -21,16 +23,16 @@ object ModelPrepariaiton {
     val testFile = datadir + "/test.csv"
 
 
-    // We do not need to wait for H2O cloud since it will be launched by backend
     val input = new H2OFrame(getSimpleCSVParser, new URI(trainFile))
     val test = new H2OFrame(getSimpleCSVParser, new URI(testFile))
 
-    val processedNames = input.names.filter( n => n.contains("_cat") || n.contains("_bin") )
+    val processedNames = input.names.filter( n => n.contains("_cat") ) ++ Array("target")//|| n.contains("_bin") ) 
     println(processedNames mkString ",")
 
     input.colToEnum(processedNames)
+    
+    test.add(Array("target"), Array(test.vec("ps_calc_15_bin").makeCopy())) //just fake column otherwise XGBoost is blowing up ;-)
     test.colToEnum(processedNames)
-
 
     val (train, valid) = split(input,0.8) // this is split 0.8/0.2
 
@@ -50,9 +52,13 @@ object ModelPrepariaiton {
     params._train = train.key
     params._valid = valid.key
     params._response_column = "target"
-//    params._ntrees=100
+    params._ntrees=200
     params._ignored_columns = Array("id")
 
+    params._stopping_rounds = 5
+    params._stopping_metric = StoppingMetric.AUC
+    params._categorical_encoding = CategoricalEncodingScheme.OneHotInternal
+    
     
     val dl = new XGBoost(params)
     dl.trainModel.get
